7f335715012aed26451a5039e62d365e01e145da
Duy Phung <duypv98@gmail.com>
2019-09-06 00:01:07 +0700

    GET request with Arguments

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,8 +3,31 @@ import { Book } from "./models/Book";
 export const resolvers = {
     Query: {
         hello: () => "Hello",
-        books: () => Book.find(),
-        totalBooks: () => Book.countDocuments()
+        totalBooks: () => Book.countDocuments(),
+        books: async (_, args) => {
+            if (args.length == 0) {
+                const _listBook = await Book.find();
+                return _listBook;
+            } else {
+                const conditions = {};
+                const { title, author, type, rating } = args;
+                if (title.length != 0) {
+                    conditions.title = title;
+                }
+                if (author.length != 0) {
+                    conditions.author = author;
+                }
+                if (type.length != 0) {
+                    conditions.type = type;
+                }
+                if (rating != null) {
+                    conditions.rating = rating;
+                }
+                const _listBook = await Book.find(conditions);
+                return _listBook;
+            }
+
+        }
     },
     Mutation: {
         createBook: async (_, { title, author, type, rating }) => {
--- src/typeDefs.js
+++ src/typeDefs.js
@@ -1,17 +1,17 @@
 import { gql } from 'apollo-server-express';
 export const typeDefs = gql`
-    type Query {
-        hello: String!
-        books: [Book!]!
-        totalBooks: Int!
-    }
-
     type Book {
         id: ID!
         title: String!
         author: String!
         type: String!
         rating: Float!
+    }
+    
+    type Query {
+        hello: String!
+        totalBooks: Int!
+        books(title: String, author: String, type: String, rating: Float): [Book]!
     }
 
     type Mutation {
